698c01d571aa5d076fdc9d5aa1cf813730ad081d
Khiem Doan <doankhiem.crazy@gmail.com>
2019-07-29 00:12:01 +0700

    Add evaluate validation set

--- train.py
+++ train.py
@@ -1,15 +1,17 @@
-import itertools
 import gc
+import itertools
+import platform
 import random
 import re
-from multiprocessing import cpu_count
+import sys
 from zipfile import ZipFile
-import platform
+
 import torch
+from sklearn.model_selection import train_test_split
 from torch import nn, optim
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, Dataset
-from tqdm import tqdm
+from tqdm import tqdm_notebook as tqdm
 
 from lang import EOS_token, Lang, SOS_token
 from model import DecoderRNN, EncoderRNN
@@ -23,8 +25,11 @@ MAX_LENGTH = 50
 LEARNING_RATE = 0.01
 HIDDEN_SIZE = 256
 TEACHER_FORCING_RATIO = 0.5
-BATCH_SIZE = 128
+BATCH_SIZE = 1024
 EPOCHS = 1000
+
+if 'google.colab' not in sys.modules:
+    BATCH_SIZE = 128
 
 
 def extract_phrases(text):
@@ -35,9 +40,12 @@ input_lang = Lang('No-tone Vietnamese')
 target_lang = Lang('Toned Vietnamese')
 
 print('Read data...')
-with open('data/vietnamese_tone_prediction.zip', 'rb') as infile:
-    with ZipFile(infile) as inzip:
-        lines = inzip.read('train.txt').decode('utf-8').split('\n')
+# with open('data/vietnamese_tone_prediction.zip', 'rb') as infile:
+#     with ZipFile(infile) as inzip:
+#         lines = inzip.read('train.txt').decode('utf-8').split('\n')
+
+with open('data/mini_train.txt', 'r', encoding='utf-8') as infile:
+    lines = infile.read().split('\n')
 
 print('Preprocess...')
 lines = itertools.chain.from_iterable(extract_phrases(line) for line in lines)
@@ -76,6 +84,20 @@ class SentenceDataSet(Dataset):
 
     def __getitem__(self, idx):
         return self.pairs[idx]
+
+
+pairs = [tensors_from_pair(pair) for pair in tqdm(pairs, 'Conv data')]
+training_pairs, validation_pairs = train_test_split(pairs, test_size=0.2)
+
+training_dataset = SentenceDataSet(training_pairs)
+training_loader = DataLoader(training_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+validation_dataset = SentenceDataSet(validation_pairs)
+validation_loader = DataLoader(validation_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+print(f'Total trains: {len(training_pairs)}')
+print(f'Total validations: {len(validation_pairs)}')
+del pairs, training_pairs, training_dataset, validation_pairs, validation_dataset
 
 
 def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion):
@@ -125,6 +147,49 @@ def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, deco
     return loss.item() / target_length
 
 
+def calc_accurate(input_tensor, target_tensor, encoder, decoder):
+    input_tensor = input_tensor.transpose(0, 1)
+
+    input_length = input_tensor.size(0)
+    target_length = target_tensor.size(1)
+
+    batch_size = input_tensor.size(1)
+
+    encoder_hidden = encoder.init_hidden(batch_size)
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
+
+    decoder_input = target_tensor[:, 0]
+    decoder_hidden = encoder_hidden
+
+    output = torch.zeros(target_length, batch_size, dtype=torch.int64, device=device)
+    output[0] = decoder_input
+
+    for di in range(1, target_length):
+        decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+        topv, topi = decoder_output.topk(1, dim=1)
+        decoder_input = topi.detach().transpose(1, 0)[0]
+        output[di] = decoder_input
+
+    total = 0
+    correct = 0
+
+    for bi in range(batch_size):
+        # print(target_lang.tensor2sentence(target_tensor[bi]))
+        # print(target_lang.tensor2sentence(output[:, bi]))
+
+        for wi in range(target_length):
+            if output[wi][bi] == target_lang.word2index[EOS_token]:
+                break
+
+            total += 1
+            if output[wi][bi] != target_tensor[bi][wi]:
+                correct += 1
+
+    return correct / total
+
+
 encoder = EncoderRNN(input_lang.n_words, HIDDEN_SIZE).to(device)
 decoder = DecoderRNN(HIDDEN_SIZE, target_lang.n_words).to(device)
 
@@ -133,19 +198,17 @@ encoder_optimizer = optim.SGD(encoder.parameters(), lr=LEARNING_RATE)
 decoder_optimizer = optim.SGD(decoder.parameters(), lr=LEARNING_RATE)
 criterion = nn.NLLLoss()
 
-training_pairs = [tensors_from_pair(pair) for pair in pairs]
-training_dataset = SentenceDataSet(training_pairs)
-training_loader = DataLoader(training_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=cpu_count())
-
 best_loss = float('inf')
+best_accurate = 0
 
 for epoch in range(EPOCHS):
     gc.collect()
 
     total_loss = 0
+    total_accurate = 0
     total_iterations = 0
 
-    desc = f'Epoch {epoch}'
+    desc = f'Train {epoch + 1}'
     for it, (input_tensor, target_tensor) in enumerate(tqdm(training_loader, desc), 1):
         loss = train(input_tensor, target_tensor,
                      encoder, decoder,
@@ -153,10 +216,18 @@ for epoch in range(EPOCHS):
                      criterion)
         total_loss += loss
         total_iterations = it
-
     avg_loss = total_loss / total_iterations
-    print('Loss: {avg_loss}')
 
-    if avg_loss < best_loss:
-        best_loss = avg_loss
-        save_model(encoder, decoder, best_loss)
+    with torch.no_grad():
+        desc = f'Valid {epoch + 1}'
+        for it, (input_tensor, target_tensor) in enumerate(tqdm(validation_loader, desc), 1):
+            total_accurate += calc_accurate(input_tensor, target_tensor, encoder, decoder)
+            total_iterations = it
+    avg_accurate = total_accurate / total_iterations
+
+    print(f'Epoch {epoch + 1}: Loss: {avg_loss}, accurate: {avg_accurate}')
+
+    if avg_accurate > best_accurate:
+        best_accurate = avg_accurate
+        print('Save to file...')
+        save_model(encoder, decoder, avg_accurate, avg_loss)
--- utils.py
+++ utils.py
@@ -1,22 +1,25 @@
 from pathlib import Path
+from typing import Tuple
 
 import torch
+from torch.nn import Module
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model_dir = Path('./models')
 
 
-def save_model(encoder, decoder, loss):
+def save_model(encoder: Module, decoder: Module, accurate: float, loss: float) -> None:
     model_dir.mkdir(parents=True, exist_ok=True)
-    model_file = model_dir / '{:.5f}.pt'.format(loss)
+    model_file = model_dir / 'accurate_{:.5f}_loss_{:.5f}.pt'.format(accurate, loss)
     torch.save({
         'encoder': encoder,
         'decoder': decoder,
+        'accurate': accurate,
         'loss': loss,
     }, model_file)
 
 
-def load_model(loss):
-    model_file = model_dir / '{:.5f}.pt'.format(loss)
+def load_model(accurate: float, loss: float) -> Tuple[Module, Module, float, float]:
+    model_file = model_dir / 'accurate_{:.5f}_loss_{:.5f}.pt'.format(accurate, loss)
     checkpoint = torch.load(model_file)
-    return checkpoint['encoder'].to(device), checkpoint['decoder'].to(device), loss
+    return checkpoint['encoder'].to(device), checkpoint['decoder'].to(device), accurate, loss
